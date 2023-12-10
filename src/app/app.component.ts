e62001a400e7e16ece15f5de740fc2f44f0b9aac
romeozor <romeozor@gmail.com>
2023-12-11 00:57:21 +0100

    add NGZorro, add JIT component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,33 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild, ViewContainerRef, ɵglobal } from '@angular/core';
+import { NzSwitchModule } from 'ng-zorro-antd/switch';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NzSwitchModule],
   template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet></router-outlet>
+    <h2>Expected</h2>
+    <nz-switch></nz-switch> {{ text }}
+    <h2>Actual</h2>
+    <ng-container #container></ng-container>
   `,
   styles: [],
 })
-export class AppComponent {
-  title = 'angular17';
+export class AppComponent implements AfterViewInit {
+  @ViewChild('container', { read: ViewContainerRef }) container!: ViewContainerRef;
+  text = new Date().toLocaleTimeString();
+
+  ngAfterViewInit(): void {
+    const html = `<nz-switch></nz-switch> {{ text }}`;
+    const parent = this;
+    
+    import('@angular/compiler').then((compiler) => {
+      compiler.publishFacade(ɵglobal); // Publish to global so its visible
+
+      const dynamicComponent = Component({template: html, jit: true, standalone: true, imports: [NzSwitchModule]})(class { text = parent.text;});
+      this.container.createComponent(dynamicComponent);
+    });
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,16 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { hu_HU, provideNzI18n } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import hu from '@angular/common/locales/hu';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+
+registerLocaleData(hu);
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideNzI18n(hu_HU), importProvidersFrom(FormsModule), importProvidersFrom(HttpClientModule), provideAnimations()]
 };
